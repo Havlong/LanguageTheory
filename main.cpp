0c021a34bf052e6bbde0309e848f96a26b2cf609
Havlong <grishin_evgenii@inbox.ru>
2020-09-14 11:35:31 +0300

    Added TODOs for language parser

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 
 
-int main() {
-
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        // TODO: implement entire program interpretation
+    } else {
+        // TODO: start live interpreter
+    }
     return 0;
 }
