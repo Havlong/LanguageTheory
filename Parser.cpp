52ab619de222488e087c44bc79421d65e668e49b
Havlong <grishin_evgenii@inbox.ru>
2020-09-14 12:18:29 +0300

    Added grammar for programming language

--- Parser.cpp
+++ Parser.cpp
@@ -19,6 +19,38 @@ std::vector<int> Parser::parseLine(std::string line) {
 
 void Parser::initializeGrammar() {
     // TODO: Initialize rules and sets for NEXT function
+    rules[PROGRAM].push_back({VAR_DECLARATION, OPERATOR_DECLARATION});
+    rules[OPERATOR_DECLARATION].push_back({'B', 'E', 'G', 'I', 'N', ASSIGNMENT_LIST, 'E', 'N', 'D'});
+    rules[VAR_DECLARATION].push_back({'V', 'A', 'R', VAR_LIST, ':', 'I', 'N', 'T', 'E', 'G', 'E', 'R', ';'});
+    rules[VAR_LIST].push_back({VAR});
+    rules[VAR_LIST].push_back({VAR, VAR_LIST});
+    rules[ASSIGNMENT_LIST].push_back({ASSIGNMENT});
+    rules[ASSIGNMENT_LIST].push_back({ASSIGNMENT, ASSIGNMENT_LIST});
+    rules[ASSIGNMENT].push_back({VAR, '=', EXPRESSION, ';'});
+    rules[EXPRESSION].push_back({UNARY_OPERATOR, SUBEXPRESSION});
+    rules[EXPRESSION].push_back({SUBEXPRESSION});
+    rules[SUBEXPRESSION].push_back({'(', EXPRESSION, ')'});
+    rules[SUBEXPRESSION].push_back({OPERAND});
+    rules[SUBEXPRESSION].push_back({SUBEXPRESSION, BINARY_OPERATOR, SUBEXPRESSION});
+    rules[UNARY_OPERATOR].push_back({'-'});
+    rules[BINARY_OPERATOR].push_back({'+'});
+    rules[BINARY_OPERATOR].push_back({'-'});
+    rules[BINARY_OPERATOR].push_back({'*'});
+    rules[OPERAND].push_back({VAR});
+    rules[OPERAND].push_back({CONST});
+    for (int i = 11; i > 0; ++i) {
+        rules[VAR + i].push_back({LETTER, VAR + i - 1});
+        rules[VAR + i].push_back({LETTER});
+    }
+    rules[VAR].push_back({LETTER});
+    rules[CONST].push_back({DIGIT});
+    rules[CONST].push_back({DIGIT, CONST});
+    for (int i = 0; i < 10; ++i) {
+        rules[DIGIT].push_back({'0' + i});
+    }
+    for (int i = 0; i < 26; ++i) {
+        rules[LETTER].push_back({'a' + i});
+    }
 }
 
 std::vector<int> Parser::parseProgram(std::string program) {
--- Parser.h
+++ Parser.h
@@ -13,22 +13,41 @@
 #include <stack>
 #include <map>
 #include <vector>
+#include <set>
 
 class Parser {
 private:
+    static const int LETTER = 327555;
+    static const int DIGIT = 327555;
+    static const int PROGRAM = 327556;
+    static const int VAR_DECLARATION = 327557;
+    static const int OPERATOR_DECLARATION = 327558;
+    static const int VAR_LIST = 327559;
+    static const int ASSIGNMENT_LIST = 327560;
+    static const int ASSIGNMENT = 327561;
+    static const int EXPRESSION = 327562;
+    static const int SUBEXPRESSION = 327563;
+    static const int UNARY_OPERATOR = 327564;
+    static const int BINARY_OPERATOR = 327565;
+    static const int OPERAND = 327566;
+    static const int CONST = 327567;
+    static const int VAR = 327568;
+
     std::stack<int> stack;
 
-    std::map<int, std::vector<int>> rules;
+    std::map<int, std::vector<std::vector<int>>> rules;
+    std::map<char, std::set<char>> first;
     std::map<char, std::map<char, int>> next;
 
     static std::vector<char> mapper;
 
-    void initializeGrammar() ;
+    void initializeGrammar();
 
 public:
 
-    std::vector<int> parseLine(std::string line) ;
-    std::vector<int> parseProgram(std::string program) ;
+    std::vector<int> parseLine(std::string line);
+
+    std::vector<int> parseProgram(std::string program);
 
 
     Parser();
